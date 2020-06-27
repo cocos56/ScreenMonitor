89300acadecca98764d6cabd30e7b760608e1013
7 <zj175@139.com>
2020-06-27 09:17:07 +0800

    更新于2020年06月27日	 9:17:07.57

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 _screenOffTimer = 300
+_hotKey = "alt + O"  # 默认情况下是alt键+英语字母表的第15个字母O键（不是数字零键）
--- monitorKeyboard.py
+++ monitorKeyboard.py
@@ -1,15 +1,21 @@
 import keyboard
-from timer import resetTimer
+from timer import resetTimer, setTimer
+from config import _screenOffTimer, _hotKey
 
 
-def callback(x):
+def hookCallback(x):
     # print(x)
     # print()
     resetTimer()
 
 
+def hotkeyCallback():
+    setTimer(_screenOffTimer)
+
+
 def monitorKeyboard():
-    keyboard.hook(callback)
+    keyboard.add_hotkey(_hotKey, hotkeyCallback)
+    keyboard.hook(hookCallback)
     # 按下任何按键时，都会调用callback，其中一定会传一个值，就是键盘事件
     keyboard.wait()
 
