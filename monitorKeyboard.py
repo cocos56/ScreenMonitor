8040eb6e704cc53c963786443dbfeedea95bf5d8
7 <zj175@139.com>
2020-07-15 11:26:36 +0800

    更新于2020年07月15日	11:26:36.88

--- monitorKeyboard.py
+++ monitorKeyboard.py
@@ -13,10 +13,15 @@ def hookCallback(x):
 def hotkeyCallback():
     screenOff()
 
+def onPressCallback(x):
+    # print(x)
+    resetTimer()
+
 
 def monitorKeyboard():
     keyboard.add_hotkey(_hotKey, hotkeyCallback)
     keyboard.hook(hookCallback)
+    keyboard.on_press(onPressCallback)
     # 按下任何按键时，都会调用callback，其中一定会传一个值，就是键盘事件
     keyboard.wait()
 
