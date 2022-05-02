677d54192a3a6df39b94fa78a5bd1f7d7ab8a230
C2 <zj175@139.com>
2022-05-02 22:39:57 +0800

    优化按快捷键息屏逻辑

--- monitorKeyboard.py
+++ monitorKeyboard.py
@@ -1,27 +1,46 @@
 import keyboard
-from timer import resetTimer
-from config import _hotKey
+from timer import resetTimer, setTimer
+from config import _hotKey, _screenOffTimer
 from screen import screenOff
+from time import sleep
+from multThread import runThread, threadLock
+
+_is_hotkey_pressed = False
 
 
-def hookCallback(x):
-    # print(x)
-    # print()
-    resetTimer()
+def get_hotkey_status():
+    return _is_hotkey_pressed
+
+
+def set_hotkey_status(is_hotkey_pressed=False):
+    global _is_hotkey_pressed
+    threadLock.acquire()
+    _is_hotkey_pressed = is_hotkey_pressed
+    threadLock.release()
 
 
 def hotkeyCallback():
+    # print('hotkeyCallback')
+    set_hotkey_status(True)
+    runThread(after_press_hotkey)
+
+
+def after_press_hotkey():
+    sleep(0.5)
+    # print('after_press_hotkey')
     screenOff()
+    setTimer(_screenOffTimer)
+    set_hotkey_status()
 
 
 def onPressCallback(x):
     # print(x)
-    resetTimer()
+    if not get_hotkey_status():
+        resetTimer()
 
 
 def monitorKeyboard():
     keyboard.add_hotkey(_hotKey, hotkeyCallback)
-    keyboard.hook(hookCallback)
     keyboard.on_press(onPressCallback)
     # 按下任何按键时，都会调用callback，其中一定会传一个值，就是键盘事件
     keyboard.wait()
