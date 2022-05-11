0479e3e7da01cfe12b817c8856580698ad6aa813
C2 <zj175@139.com>
2022-05-11 20:35:58 +0800

    增加对哔哩哔哩全屏播放时异常息屏情况的处理

--- excluded_programs.py
+++ excluded_programs.py
@@ -1,7 +1,9 @@
 import pywintypes
 import win32gui
 
-_excluded_programs = [{'name': '铁甲雄兵', 'classname': 'TNG8SK2K8E', 'title': '铁甲雄兵'}]
+full_screen_rectangles = [(0, 0, 1536, 864), (-1, -1, 1537, 865)]
+_excluded_programs = [{'name': '铁甲雄兵', 'classname': 'TNG8SK2K8E', 'title': '铁甲雄兵'},
+                      {'name': '哔哩哔哩', 'classname': 'Chrome_WidgetWin_1', 'title': '哔哩哔哩'}]
 
 
 def is_current_window_excluded_program():
@@ -10,7 +12,8 @@ def is_current_window_excluded_program():
         classname = win32gui.GetClassName(current_window)
         title = win32gui.GetWindowText(current_window)
         for i in _excluded_programs:
-            if i['classname'] == classname and i['title'] in title:
+            if i['classname'] == classname and i['title'] in title \
+                    and win32gui.GetWindowRect(current_window) in full_screen_rectangles:
                 print(f'{i["name"]}正在阻止息屏')
                 return True
     except pywintypes.error as e:
