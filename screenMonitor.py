73496af3162cf5ea3cb0fa03b4045fc4cc75fec3
C2 <zj175@139.com>
2022-05-11 09:26:33 +0800

    增加命令行参数解析处理

--- screenMonitor.py
+++ screenMonitor.py
@@ -2,7 +2,20 @@ from multThread import runThread
 from monitorMouse import monitorMouse
 from monitorKeyboard import monitorKeyboard
 from timer import updateTimer
+import argparse
 
-runThread(monitorMouse)
-runThread(monitorKeyboard)
-runThread(updateTimer)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--version", help="Show the current version.",
+                    action="store_true")
+
+args = parser.parse_args()
+if args.mute:
+    print("The current version is v0.0.1")
+    exit(0)
+
+
+if __name__ == "__main__":
+    runThread(monitorMouse)
+    runThread(monitorKeyboard)
+    runThread(updateTimer)
