cefc134fb2bcc43e943e7ec3abf1f6aa2a4479b5
C2 <zj175@139.com>
2022-05-11 09:25:41 +0800

    架构调整

--- mute.py
+++ mute.py
@@ -1,12 +1,9 @@
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import argparse
 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-
-volume = cast(interface, POINTER(IAudioEndpointVolume))
+volume = cast(AudioUtilities.GetSpeakers().Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None),
+              POINTER(IAudioEndpointVolume))
 
 
 def setMute(flag=1):
@@ -14,14 +11,6 @@ def setMute(flag=1):
         return
     volume.SetMute(flag, None)
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--mute", help="Show the current version.",
-                    action="store_true")
-
-args = parser.parse_args()
-if args.mute:
-    print("The current version is v1.0")
 
 if __name__ == "__main__":
     setMute()
